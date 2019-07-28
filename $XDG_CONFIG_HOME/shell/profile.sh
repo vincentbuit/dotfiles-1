6d1cbcf23755962239ea48e471d4a1dec01f56ca
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2019-07-28 22:28:14 +0200

    Make gpg setup quieter

--- $XDG_CONFIG_HOME/shell/profile.sh
+++ $XDG_CONFIG_HOME/shell/profile.sh
@@ -78,7 +78,7 @@ fi
 
 # Set-up ----------------------------------------------------------------------
 chmod go-rwx "$GNUPGHOME" >/dev/null 2>&1
-if [ "$(gpg --list-secret-keys | wc -l)" = 0 ] \
+if [ $(gpg --list-secret-keys | wc -l 2>/dev/null) = 0 ] \
     && [ -f "$GNUPGHOME/keys.asc" ]; then
     killall -s SIGKILL gpg-agent >/dev/null 2>&1
     #generate with gpg --export-secret-keys -a >"$GNUPGHOME/keys.asc"
