b2314a174607fafc36afcfb81e9629fb4253b430
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2019-11-19 13:40:14 +0100

    Fix starting multiple mail daemons

--- $XDG_CONFIG_HOME/sh/profile.sh
+++ $XDG_CONFIG_HOME/sh/profile.sh
@@ -96,8 +96,8 @@ mergehistory() {
 (pgrep deluged || deluged&) >/dev/null 2>&1
 (pgrep shairport-sync || shairport-sync&) >/dev/null 2>&1
 (pgrep -f yt_music_log || yt_music_log&) >/dev/null 2>&1
+(pgref -f 'm daemon' || m daemon&) >/dev/null 2>&1
 (winvm up&) >/dev/null 2>&1
-(m daemon&) >/dev/null 2>&1
 
 # Start X ---------------------------------------------------------------------
 [ -z "$DISPLAY" ] \
