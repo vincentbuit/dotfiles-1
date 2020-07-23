c023949389b103cec4faf40761138e4dea13addd
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2020-07-23 21:04:00 +0200

    Change from hotkeys to window switcher on macOS

--- $XDG_CONFIG_HOME/hammerspoon/init.lua
+++ $XDG_CONFIG_HOME/hammerspoon/init.lua
@@ -10,43 +10,6 @@ hs.window.filter.ignoreAlways['Electron Helper (Renderer)'] = true
 hs.window.filter.ignoreAlways['Mail Networking'] = true
 configWatcher = hs.pathwatcher.new(hs.configdir, hs.reload):start()
 
--- Bind switchers. I want them to be blindingly fast, so maximize caching
-function createSwitcher(filter)
-   return hs.window.switcher.new(
-      hs.window.filter.new(filter),
-      {
-         backgroundColor = {0.03, 0.03, 0.03, 0.75},
-         highlightColor = {0.6, 0.3, 0.0, 0.75},
-         showThumbnails = false,
-         showSelectedTitle = false,
-         showTitles = false
-      }
-   )
-end
-
-function bindSwitcher(mods, key, launchBundleID, createFunc)
-   local switcher = createFunc()
-   hs.hotkey.bind(mods, key, function()
-      switcher:next()
-      if hs.application.get(launchBundleID) == nil then
-         hs.application.launchOrFocusByBundleID(launchBundleID)
-         createFunc()
-      end
-   end)
-end
-
-bindSwitcher({"cmd", "shift"}, "Space", "com.apple.Terminal", function()
-   return createSwitcher({"Terminal"})
-end)
-
-bindSwitcher({"cmd"}, "m", "com.apple.Music", function()
-   return createSwitcher({"Music", "Spotify"})
-end)
-
-bindSwitcher({"cmd"}, "d", "com.microsoft.VSCode", function()
-   return createSwitcher({"Code", "Remote Desktop"})
-end)
-
 -- Cmd + I
 function runOrRaise(bundleID, names)
     local windows = hs.window.filter.new(names):getWindows()
@@ -60,3 +23,65 @@ end
 hs.hotkey.bind({"cmd"}, "I", function()
     runOrRaise("com.apple.Safari", {"Safari"})
 end)
+
+-- Cmd + Space
+function retry_nil(n, func)
+    local rval = nil
+    repeat
+        rval = func()
+        n = n - 1
+    until rval ~= nil or n <= 0
+    return rval
+end
+
+function retry_p(n, func)
+    local rval = nil
+    repeat
+        n = n - 1
+    until pcall(function() rval = func() end) or n <= 0
+    return rval
+end
+
+function windowById(id)
+    return hs.fnutils.find(
+        allWindows(),
+        function(x)
+            return x:id() == id
+        end
+    )
+end
+
+function allWindows()
+    return hs.fnutils.concat(
+        retry_p(9, function() return hs.window.filter.new() end):getWindows(),
+        retry_p(0, function()
+            return hs.application.get("com.microsoft.VSCode"):allWindows()
+        end) or {}
+    )
+end
+
+chooser = hs.chooser.new(function(x)
+    if x ~= nil and x.id ~= nil then
+        local window = retry_nil(0, function() return windowById(x.id) end)
+        if window ~= nil then
+            window:focus()
+        else
+            print("wtf")
+        end
+    end
+end):searchSubText(true):choices(function()
+    return hs.fnutils.imap(
+        allWindows(),
+        function(x)
+            local subText = x:application():name()
+            if subText == "Safari" then subText = "Browser" end
+            return {
+                ["text"] = x:title(),
+                ["subText"] = subText,
+                ["id"] = x:id()
+            }
+        end
+    )
+end)
+
+hs.hotkey.bind({"cmd"}, "Space", function() chooser:query(""):refreshChoicesCallback():show() end)
